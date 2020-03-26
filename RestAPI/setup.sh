4527c0ff6607552d849419aadee230563588dc13
Tom Lou <louyu27@cs.washington.edu>
2020-03-26 15:44:45 -0700

    Remove unessesary codes

--- RestAPI/setup.sh
+++ RestAPI/setup.sh
@@ -6,13 +6,8 @@ cd $WORK
 # setup extra python libs	
 NEWPY2PATH=$LOCAL/lib/python2.7/site-packages	
 mkdir -p $NEWPY2PATH	
-# need --ignore-installed or it tries to uninstall cvmfs packages	
-pip install --prefix $LOCAL --no-cache-dir --ignore-installed wheel setuptools grpcio-tools	
+# need --ignore-installed or it tries to uninstall cvmfs packages		
 export PYTHON27PATH=$NEWPY2PATH:$PYTHON27PATH	
 
 # to get cmake	
 export PATH=/cvmfs/sft.cern.ch/lcg/contrib/CMake/3.7.0/Linux-x86_64/bin/:${PATH}	
-
-# some really bad ways to get info out of scram	
-PYTHON_LIBDIR=$(scram tool info python | grep "LIBDIR=" | sed 's/LIBDIR=//')	
-PYTHON_INCLUDE=$(scram tool info python | grep "INCLUDE=" | sed 's/INCLUDE=//')
