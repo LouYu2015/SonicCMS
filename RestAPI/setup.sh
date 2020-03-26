838fbb31c80d7fe7398a5f14aabcad3a11c2063c
Tom Lou <louyu27@cs.washington.edu>
2020-03-26 15:41:59 -0700

    Add setup back

--- RestAPI/setup.sh
+++ RestAPI/setup.sh
@@ -1,3 +1,18 @@
 #!/bin/bash -e
 
+cd $WORK	
 
+
+# setup extra python libs	
+NEWPY2PATH=$LOCAL/lib/python2.7/site-packages	
+mkdir -p $NEWPY2PATH	
+# need --ignore-installed or it tries to uninstall cvmfs packages	
+pip install --prefix $LOCAL --no-cache-dir --ignore-installed wheel setuptools grpcio-tools	
+export PYTHON27PATH=$NEWPY2PATH:$PYTHON27PATH	
+
+# to get cmake	
+export PATH=/cvmfs/sft.cern.ch/lcg/contrib/CMake/3.7.0/Linux-x86_64/bin/:${PATH}	
+
+# some really bad ways to get info out of scram	
+PYTHON_LIBDIR=$(scram tool info python | grep "LIBDIR=" | sed 's/LIBDIR=//')	
+PYTHON_INCLUDE=$(scram tool info python | grep "INCLUDE=" | sed 's/INCLUDE=//')
